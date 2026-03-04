83dc9fe85dddefb4b7bbd7d437fb24f31dc0d864
Jean-Paul Nahirwe <nahirwej@gmail.com>
2026-03-05 01:27:08 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Michael Brown PHP</title>
+</head>
+
+<body>
+    <h1>Michael Brown PHP</h1>
+
+    <pre>
+    M       M
+    MM     MM
+    M M   M M
+    M  M M  M
+    M   M   M
+    M       M
+    M       M
+    M       M
+    </pre>
+
+    <p>The SHA256 hash of "Michael Brown" is:</p>
+    <pre>
+    <?php
+    print hash('sha256', 'Michael Brown');
+    ?>
+    </pre>
+</body>
+
+</html>
